dcb0928f6d67472d734e0a70886bb883e8525a33
hristiyana <96460833+hristiyana01@users.noreply.github.com>
2023-08-19 23:33:17 +0300

    separating into methods

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,11 +1,22 @@
 package org.example;
 
 public class Car {
-    int topSpeed;
+    static int topSpeed = 100;
     int totalSeats;
-    int fuelCapacity;
+    final int fuelCapacity = 30;
+    static int maxCapacity = 4;
     String manufacturer;
 
+    public int printSpeed(int speed) {
+        //System.out.println("The speed is: " + speed);
+        return speed + 5;
+    }
+    public void setTopSpeep(int speed) {
+        topSpeed = speed;
+    }
+    public int getTopSpeed() {
+        return topSpeed;
+    }
     void refuel() {
         //int fuel = 4;
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,16 @@ public class Main {
 
         Cop c = new Cop();
         c.getGun();
+
+        Car car = new Car();
+        System.out.println(car.manufacturer);
+        System.out.println(car.fuelCapacity);
+
+        car.printSpeed(200);
+
+        System.out.println(Car.topSpeed);
+        System.out.println(Car.maxCapacity);
+
+        System.out.println(car.getTopSpeed());
     }
 }
